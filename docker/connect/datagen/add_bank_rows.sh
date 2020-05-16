bc1f5c9217ba38f846607cd378bb2a4d1fe02afa
Lim Swee Tat <sweetat+github@xfers.com>
2020-05-16 20:42:53 +0800

    Adding bank_rows properly

--- docker/connect/datagen/add_bank_rows.sh
+++ docker/connect/datagen/add_bank_rows.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 
-if [ ! -f docker/connect/datagen/bank_rows.config ]; then
-  echo "Wrong directory.  Pls run in the main project directory"
-fi
 
-## This is for the datagen to generate insane numbers of data for testing
-curl -X POST \
-  -H "Content-Type: application/json" \
-  --data @docker/connect/datagen/bank_rows.config \
-  http://localhost:8083/connectors
+CONNECTOR_NAME=datagen-bankrows
+CONFIG_FILE=/connect/datagen/bank_rows.config
+
+/connect/add_connector.sh ${CONNECTOR_NAME} ${CONFIG_FILE}
